db9f0dda5bdc6833b2fefabbe7cdd9616c6edaed
Janani <janani.jayanthan@publicissapient.com>
2019-12-02 14:27:54 +0000

    deleted some files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app";
-import "./styles.css";
-
-
-var mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Jane" />, mountNode);
